86a71b9f76bfcecd5d62e65d1644d37e6746f6ab
Atik1000 <foysalkazi45@gmail.com>
2022-01-15 23:08:56 +0600

    basic go and python file setup

---
+++ class-1/loop.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+func loop() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+}
+func main() {
+	loop()
+}
---
+++ class-1/loop.py
@@ -0,0 +1,4 @@
+def loop():
+    for i in range(10):
+        print(i)
+loop()
