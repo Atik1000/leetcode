d1e315dad52f040fc73fb868a567cb4a32f7412f
Atik1000 <foysalkazi45@gmail.com>
2022-01-19 00:33:12 +0600

    array added

---
+++ class-1/switch.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	i := 2
+	fmt.Println("i is", i)
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's weekend")
+	default:
+		fmt.Println("It's week day")
+
+	}
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a boolean")
+		case int:
+			fmt.Println("I'm int")
+		default:
+			fmt.Println("Don't know type %T\n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
+}
